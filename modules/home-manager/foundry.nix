729bf22922078059eab4bd541e92f375fcdb90e2
Dragonlinev2 <pawellos100@gmail.com>
2025-11-08 08:25:34 +0100

    Foundry config updated

--- modules/home-manager/foundry.nix
+++ modules/home-manager/foundry.nix
@@ -1,9 +1,20 @@
-{ inputs, ... }:
+{ inputs, pkgs, ... }:
+
 {
-	services.foundryvtt = {
-		enable = true;
-		hostName = "myhostname.com";
-		proxySSL = true;
-		proxyPort = 443;
-	};
+  services.foundryvtt = {
+    enable = true;
+
+    # Correct way: call the foundryvtt function with src
+    package = inputs.foundryvtt.packages.x86_64-linux.foundryvtt {
+      src = pkgs.requireFile {
+        name = "FoundryVTT-Linux-13.350.zip";
+        url = "https://foundryvtt.com";
+        sha256 = "sha256-CIc4CxqGmYviAUp76L8pwo+CVZ4cKyl2RcQ7qjlvkOo=";
+      };
+    };
+
+    hostName = "dragon-den";
+    proxySSL = true;
+    proxyPort = 443;
+  };
 }
