42217d7b27e35eba1ba9f1f38fd85d33587e5b8a
Dragonlinev2 <pawellos100@gmail.com>
2025-11-08 08:31:18 +0100

    Foundry config updated

--- modules/home-manager/foundry.nix
+++ modules/home-manager/foundry.nix
@@ -1,19 +1,18 @@
-
 { inputs, pkgs, ... }:
 
+let
+  foundryPkg = inputs.foundryvtt.packages.${pkgs.system}.default {
+    src = pkgs.requireFile {
+      name = "FoundryVTT-Linux-13.350.zip";
+      url = "https://foundryvtt.com";
+        sha256 = "sha256-CIc4CxqGmYviAUp76L8pwo+CVZ4cKyl2RcQ7qjlvkOo=";
+    };
+  };
+in
 {
   services.foundryvtt = {
     enable = true;
-
-    # Correct for nixpkgs-style Foundry package:
-    package = pkgs.foundryvtt.override {
-      src = pkgs.requireFile {
-        name = "FoundryVTT-Linux-13.350.zip";
-        url = "https://foundryvtt.com";
-        sha256 = "sha256-CIc4CxqGmYviAUp76L8pwo+CVZ4cKyl2RcQ7qjlvkOo=";
-      };
-    };
-
+    package = foundryPkg;
     hostName = "dragon-den";
     proxySSL = true;
     proxyPort = 443;
